57333a4b13f4d5763c1a98954758cf32a4b16227
Madhu Srinivasan <msrinivasa@cray.com>
2020-09-18 21:03:46 -0500

    updates to batchscript

--- slurm/network_test.sh
+++ slurm/network_test.sh
@@ -11,19 +11,20 @@
 #SBATCH --requeue
 # maximum job time in HH:MM:SS
 #SBATCH --time=06:00:00
-#SBATCH --nodes=5
+#SBATCH --nodes=10
 # maximum memory
 #SBATCH --mem-per-cpu=1024
 # run a single task
-#SBATCH --ntasks=50
+#SBATCH --ntasks=2560
 #SBATCH --cpus-per-task=1
 ###
 
+module restore PrgEnv-cray
 
-module load cce/10.0.3
-module load craype/2.7.2
-module load cray-mpich/8.0.15
-module load cray-libsci/20.08.1.2
+# module load cce/10.0.3
+# module load craype/2.7.2
+# module load cray-mpich/8.0.15
+# module load cray-libsci/20.08.1.2
 
 cd /home/users/msrinivasa/develop/GPCNET
 
